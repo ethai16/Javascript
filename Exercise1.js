5f4021c8864cede82d8b43aea1c0faa9383f7c64
ethai16 <erick.thai16@gmail.com>
2018-12-04 13:36:32 -0600

    first commit

---
+++ Exercise1.js
@@ -0,0 +1,57 @@
+
+function lvl1exercise1() {
+	// Declare a variable without instantiating it and return it!
+    "erick"
+    return "erick"
+}
+
+console.log(lvl1exercise1());
+
+
+function lvl1exercise2() {
+	// Declare and instantiate a number and return it
+    var a = 123
+    return a
+}
+
+console.log(lvl1exercise2());
+
+function lvl1exercise3() {
+	// Declare and instantiate a floating point number that isn't a whole number and return it
+    var b = 1.4
+    return b
+}
+
+console.log(lvl1exercise3());
+
+function lvl1exercise4() {
+	// Declare and instantiate a string "Hello World!" and return it
+    var string = "Hello World!"
+    return string
+}
+
+console.log(lvl1exercise4());
+
+function lvl1exercise5() {
+	// Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+    var myArray = ["Hello World!", 4]
+    return myArray
+}
+console.log(lvl1exercise5());
+
+function lvl1exercise6() {
+	// Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+    var myObject = {
+        key1: "Hello World!",
+        key2: 4
+    }
+    return myObject
+}
+console.log(lvl1exercise6());
+
+function lvl1exercise7() {
+	// Declare and instantiate a boolean value 'false' and return it
+    var likeCats = False
+    return likeCats
+}
+console.log(lvl1exercise7())
