7472d476b6b05540d47551502de52b0551295b80
ethai16 <erick.thai16@gmail.com>
2018-12-04 13:44:56 -0600

    ex.2

--- Exercise2.js
+++ Exercise2.js
@@ -1,13 +1,13 @@
 function lvl2exercise1(num1, num2) {
 	// Return the sum of num1 and num2
-    sum = num1 + num2
+    var sum = num1 + num2
     return sum
 }
 console.log(lvl2exercise1(1,2))
 
 function lvl2exercise2(num1, num2) {
 	// Return the difference of num1 and num2
-    sub = num1 - num2
+    var sub = num1 - num2
     return sub
 }
 
@@ -15,14 +15,14 @@ console.log(lvl2exercise2(3,4));
 
 function lvl2exercise3(num1, num2) {
 	// Return the multiplication of num1 and num 2
-    mult = num1*num2
+    var mult = num1*num2
     return mult
 }
 console.log(lvl2exercise3(3,4));
 
 function lvl2exercise4(num1, num2) {
 	// Return the division of num1 and num2
-    div = num1/num2
+    var div = num1/num2
     return div
 }
 
@@ -30,7 +30,7 @@ console.log(lvl2exercise4(10,2));
 
 function lvl2exercise5(num1) {
 	// Add 2 to num1 using += and return it
-    num1 += 2
+    var num1 += 2
     return num1
 }
 
